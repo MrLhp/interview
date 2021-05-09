9799c644fecca1207b8319b2fc889806f4e74698
haipei-laptop <happy612@vip.qq.com>
2021-05-09 09:54:25 +0800

    init

--- src/main/java/com/interview/study/basic/aspose/AsposeCrack.java
+++ src/main/java/com/interview/study/basic/aspose/AsposeCrack.java
@@ -1,5 +1,21 @@
-package com.interview.study.basic.aspose;
-
-public class AsposeCrack {
-
-}
+//package com.interview.study.basic.aspose;
+//
+//import org.apache.ibatis.javassist.*;
+//
+//import java.io.IOException;
+//
+//public class AsposeCrack {
+//    //C:\Users\happy\Downloads
+//    public static void main(String[] args) {
+//        try {
+//            ClassPool.getDefault().insertClassPath("C:\\Users\\happy\\Downloads\\aspose-slides-20.4-jdk16.jar");
+//            CtClass ctClass = ClassPool.getDefault().getCtClass("com.aspose.slides.y6");
+//            ctClass.getDeclaredMethod("if").setName("tempListIf");
+//            ctClass.getDeclaredMethod("if").setBody("{return 1;}");
+//            ctClass.getDeclaredMethod("tempListIf").setName("if");
+//            ctClass.writeFile("d:\\test\\");
+//        } catch (NotFoundException | CannotCompileException | IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
--- src/main/java/com/interview/study/basic/aspose/AsposeWordTest.java
+++ src/main/java/com/interview/study/basic/aspose/AsposeWordTest.java
@@ -1,43 +1,43 @@
-package com.interview.study.basic.aspose;
-
-import com.aspose.words.*;
-import org.springframework.util.ResourceUtils;
-
-import java.io.FileNotFoundException;
-
-public class AsposeWordTest {
-    public static void main(String[] args) {
-        try {
-//            File file = ResourceUtils.getFile("classpath:word/template.docx");
-            Document doc = new Document();
-
-// Tables are placed in the body of a document
-            Table table = new Table(doc);
-            doc.getFirstSection().getBody().appendChild(table);
-
-// Tables contain rows, which contain cells,
-// which contain contents such as paragraphs, runs and even other tables
-// Calling table.EnsureMinimum will also make sure that a table has at least one row, cell and paragraph
-            Row firstRow = new Row(doc);
-            table.appendChild(firstRow);
-
-            Cell firstCell = new Cell(doc);
-            firstRow.appendChild(firstCell);
-
-            Paragraph paragraph = new Paragraph(doc);
-            firstCell.appendChild(paragraph);
-
-            Run run = new Run(doc, "Hello world!");
-            paragraph.appendChild(run);
-
-            doc.save(getArtifactsDir() + "Table.CreateTable.docx");
-            System.out.println(ResourceUtils.getFile("classpath:word/").getPath());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static String getArtifactsDir() throws FileNotFoundException {
-        return ResourceUtils.getFile("classpath:word/").getPath()+"/";
-    }
-}
+//package com.interview.study.basic.aspose;
+//
+//import com.aspose.words.*;
+//import org.springframework.util.ResourceUtils;
+//
+//import java.io.FileNotFoundException;
+//
+//public class AsposeWordTest {
+//    public static void main(String[] args) {
+//        try {
+////            File file = ResourceUtils.getFile("classpath:word/template.docx");
+//            Document doc = new Document();
+//
+//// Tables are placed in the body of a document
+//            Table table = new Table(doc);
+//            doc.getFirstSection().getBody().appendChild(table);
+//
+//// Tables contain rows, which contain cells,
+//// which contain contents such as paragraphs, runs and even other tables
+//// Calling table.EnsureMinimum will also make sure that a table has at least one row, cell and paragraph
+//            Row firstRow = new Row(doc);
+//            table.appendChild(firstRow);
+//
+//            Cell firstCell = new Cell(doc);
+//            firstRow.appendChild(firstCell);
+//
+//            Paragraph paragraph = new Paragraph(doc);
+//            firstCell.appendChild(paragraph);
+//
+//            Run run = new Run(doc, "Hello world!");
+//            paragraph.appendChild(run);
+//
+//            doc.save(getArtifactsDir() + "Table.CreateTable.docx");
+//            System.out.println(ResourceUtils.getFile("classpath:word/").getPath());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public static String getArtifactsDir() throws FileNotFoundException {
+//        return ResourceUtils.getFile("classpath:word/").getPath()+"/";
+//    }
+//}
